3df3d64d93bb62c6b614761a3c57c5fbded523f4
wb_caizhixiang <wb_caizhixiang@zhongan.com>
2017-10-25 16:18:22 +0800

    统一异常处理

--- springboot_mybatis_springboot/src/main/java/com/caizhixiang/web/exception/ApplicationControllerExceptionHandler.java
+++ springboot_mybatis_springboot/src/main/java/com/caizhixiang/web/exception/ApplicationControllerExceptionHandler.java
@@ -5,14 +5,17 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import lombok.extern.slf4j.Slf4j;
-
+@EnableWebMvc
 @ControllerAdvice
 @Slf4j
 public class ApplicationControllerExceptionHandler {
@@ -28,4 +31,16 @@ public class ApplicationControllerExceptionHandler {
 	        map.put("status", "0");
 	        return map;
 	    }
+	 	@ExceptionHandler(value = { NoHandlerFoundException.class })
+	 	@ResponseStatus(HttpStatus.NOT_FOUND)
+	 	@ResponseBody
+	 	public Map<String, Object> handlerError1(HttpServletRequest req, Exception e) {
+	 		Map<String,Object> map=new HashMap<>();
+	 		map.put("tip", "此错误说明调用接口失败，失败原因见msg，如果msg为空，联系后台");
+	 		map.put("msg", e.getMessage());
+	 		map.put("path", req.getRequestURI());
+	 		map.put("params", req.getParameterMap());
+	 		map.put("status", "0");
+	 		return map;
+	 	}
 }

3ee8e27e5698c8c717b8e0093ad71900749cd350
wb_caizhixiang <wb_caizhixiang@zhongan.com>
2017-10-26 18:03:51 +0800

    springboot-parent

--- springboot_mybatis_springboot/src/main/java/com/caizhixiang/web/exception/WebConfig.java
+++ springboot_mybatis_springboot/src/main/java/com/caizhixiang/web/exception/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 	@Bean
